94d3229e5bf2e72204ef1447a1c6f0c9916ca578
mrbengutierrez <mrbengutierrez@gmail.com>
2020-05-20 19:38:39 -0400

    testing

--- shared-resources/shared-head.js
+++ shared-resources/shared-head.js
@@ -31,7 +31,7 @@ document.write(
  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=yes">\
  \
  <!-- Favicon -->\
- <link rel="icon" href="https://drive.google.com/uc?export=view&id=1w3suWEy1V0XilYO9JNMJDEnDkWZvOGze">\
+ <link rel="icon" href="https://drive.google.com/uc?export=view&id=11b0YtsxHhqw7KIzV239aschfVTan7It-">\
  \
  <!-- Google Fonts -->\
  <link href="https://fonts.googleapis.com/css2?family=Josefin+Slab:ital,wght@0,100;0,300;0,400;0,600;0,700;1,100;1,300;1,400;1,600;1,700&family=Raleway:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">\
