e591f3b3052edf62cb6c86d5ce025d97274a97d2
mrbengutierrez <mrbengutierrez@gmail.com>
2020-05-20 02:09:32 -0400

    added education

--- navigation.js
+++ navigation.js
@@ -1,3 +1,7 @@
+// Shared navigation bar for all pages
+
+
+
 var html = '<nav class="navbar navbar-expand-md navbar-dark bg-dark navigation-bar">\
     <a class="navbar-brand" href="www.haploman.com">Haploman</a>\
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarToggler_01" aria-controls="navbarToggler_01" aria-expanded="false" aria-label="Toggle navigation">\
@@ -6,13 +10,13 @@ var html = '<nav class="navbar navbar-expand-md navbar-dark bg-dark navigation-b
     <div class="collapse navbar-collapse" id="navbarToggler_01">\
       <ul class="navbar-nav ml-auto">\
         <li class="nav-item">\
-          <a class="nav-link" href="www.haploman.com/education">Education</a>\
+          <a class="nav-link" href="http://www.haploman.com/education">Education</a>\
         </li>\
         <li class="nav-item">\
-          <a class="nav-link" href="www.haploman.com/projects">Projects</a>\
+          <a class="nav-link" href="http://www.haploman.com/projects">Projects</a>\
         </li>\
         <li class="nav-item">\
-          <a class="nav-link" href="www.haploman.com/#contact">Contact</a>\
+          <a class="nav-link" href="http://www.haploman.com/#contact">Contact</a>\
         </li>\
       </ul>\
     </div>\
