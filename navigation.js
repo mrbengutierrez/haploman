54ab4d45043846b7353125c355db741c4f78585d
mrbengutierrez <mrbengutierrez@gmail.com>
2020-05-20 03:03:53 -0400

    added education

--- navigation.js
+++ navigation.js
@@ -5,25 +5,25 @@
 
 
 var html = '<nav class="navbar navbar-expand-md navbar-dark bg-dark navigation-bar">\
-    <a class="navbar-brand" href="www.haploman.com">Haploman</a>\
+    <a class="navbar-brand" href="https://www.haploman.com">Haploman</a>\
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarToggler_01" aria-controls="navbarToggler_01" aria-expanded="false" aria-label="Toggle navigation">\
       <span class="navbar-toggler-icon"></span>\
     </button>\
     <div class="collapse navbar-collapse" id="navbarToggler_01">\
       <ul class="navbar-nav ml-auto">\
         <li class="nav-item">\
-          <a class="nav-link" href="http://www.haploman.com/education">Education</a>\
+          <a class="nav-link" href="https://www.haploman.com/education">Education</a>\
         </li>\
         <li class="nav-item">\
-          <a class="nav-link" href="http://www.haploman.com/projects">Projects</a>\
+          <a class="nav-link" href="https://www.haploman.com/projects">Projects</a>\
         </li>\
         <li class="nav-item">\
-          <a class="nav-link" href="http://www.haploman.com/#contact">Contact</a>\
+          <a class="nav-link" href="https://www.haploman.com/#contact">Contact</a>\
         </li>\
       </ul>\
     </div>\
   </nav>\
-  <div class="navigation-spacing"></div>';
+  <div class="navigation-spacing"><br></div>';
 
 
 document.write(html);
