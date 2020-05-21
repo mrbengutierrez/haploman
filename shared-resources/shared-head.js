a3df4ed4b5c750f6045a71c3f5a51a34c47eb0e5
mrbengutierrez <mrbengutierrez@gmail.com>
2020-05-20 21:19:00 -0400

    changed link to shared resources

--- shared-resources/shared-head.js
+++ shared-resources/shared-head.js
@@ -40,4 +40,4 @@ document.write(
  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">\
  \
  <!-- External Universal Stylesheet -->\
- <link rel="stylesheet" type="text/css" href="https://haploman.com/universal-styles.css">');
+ <link rel="stylesheet" type="text/css" href="https://haploman.com/shared-resources/shared-styles.css">');
