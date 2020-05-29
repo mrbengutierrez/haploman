44a4d8c68238d4380656f542a67bdaa171a89dec
mrbengutierrez <mrbengutierrez@gmail.com>
2020-05-29 17:14:59 -0400

    adding google analytics

--- shared-resources/shared-head.js
+++ shared-resources/shared-head.js
@@ -41,4 +41,14 @@ document.write(
  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">\
  \
  <!-- External Universal Stylesheet -->\
- <link rel="stylesheet" type="text/css" href="https://haploman.com/shared-resources/shared-styles.css">');
+ <link rel="stylesheet" type="text/css" href="https://haploman.com/shared-resources/shared-styles.css">\
+\
+ <!-- Global site tag (gtag.js) - Google Analytics -->\
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-168040169-1"></script>\
+<script>\
+  window.dataLayer = window.dataLayer || [];\
+  function gtag(){dataLayer.push(arguments);}\
+  gtag("js", new Date());\
+\
+  gtag("config", "UA-168040169-1");\
+</script>');
